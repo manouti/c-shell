830b1b46b8ad31cb103cc6a87d520e8f130e5c7e
mahmoud.anouti <mahmoud.anouti@d2ccdeb4-faeb-767e-6b92-2c8aa4952157>
2010-12-09 15:32:03 +0000

    upload src

---
+++ trunk/Guolice/src/Node.cpp
@@ -0,0 +1,91 @@
+#include "Node.h"
+
+Node::Node() {}
+
+Node::Node(string v)
+{
+	value = v;
+}
+
+Node::Node(Node* node)
+{
+	this->value = node->value;
+	this->children = node->children;
+	this->parent = node->parent;
+}
+
+string Node::getValue() const
+{
+	return value;
+}
+
+void Node::setValue(string v)
+{
+	value = v;
+}
+
+Node::NodeType Node::getType() const
+{
+	return type;
+}
+
+void Node::setType(Node::NodeType type)
+{
+	this->type = type;
+}
+
+vector<Node*> Node::getChildren() const
+{
+	return children;
+}
+
+void Node::setChildren(vector<Node*> c)
+{
+	children = c;
+}
+
+Node* Node::getParent() const
+{ 
+	return parent;
+}
+
+void Node::setParent(Node* p)
+{
+	parent = p;
+}
+
+void Node::addChild(Node* child)
+{
+	children.push_back(child);
+	child->setParent(this);
+}
+
+string Node::printChildren() const
+{
+	string childrenString = "";
+	for(int i = 0; i < children.size(); i++) {
+		if(i == 0) {
+			childrenString += children[0]->getValue();
+		}
+				
+		else {
+			childrenString += ", ";
+			childrenString += children[i]->getValue();
+		}
+	}
+	
+	if (childrenString == ""|| childrenString == " ")
+		childrenString = "NULL";    			
+	return childrenString;
+}
+
+string Node::toString() const
+{
+	string nodeString ("Node \t ");
+	nodeString += value + "\n\t\t parent \t-> ";
+	nodeString += ((parent == NULL)?"NULL":parent->getValue());
+	nodeString += "\n\t\t children \t-> ";
+	nodeString += printChildren() + "";
+	
+	return nodeString;
+}
