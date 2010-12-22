70425a559ef2c32d832ade499eac93128398896f
lama.ghusn <lama.ghusn@d2ccdeb4-faeb-767e-6b92-2c8aa4952157>
2010-12-22 13:52:24 +0000

    commiting aspect changes

--- trunk/Guolice/include/ParseTree.h
+++ trunk/Guolice/include/ParseTree.h
@@ -1,7 +1,3 @@
-/** \file ParseTree.h
-*
-*/
-
 #ifndef PARSETREE_H
 #define PARSETREE_H
 
@@ -11,6 +7,7 @@
 #include <map>
 #include <sstream>
 #include <algorithm>
+#include <iostream>
 #include "Node.h"
 
 using namespace std;
@@ -19,6 +16,7 @@ using namespace std;
 class ParseTree {
 
 	public:
+                ParseTree(){}
 		ParseTree(Node *root);
 		  // Constructor using a start node.
 		  
@@ -29,10 +27,10 @@ class ParseTree {
 		  // Determines whether the tree is empty.
 		  // Postcondition: Returns true if the tree is empty, false otherwise.
 		  
-		void traverse(Node *node);
-		  // Traversal function for the tree structure. It has calls to visitNode and visitEdge.
-		  // Postcondition: The specified node and all its children are visited in a recursive way.
-		
+                void traversing(Node* node);
+                  // Traversal function for the tree structure. It has calls to visitNode and visitEdge.
+                  // Postcondition: The specified node and all its children are visited in a recursive way.
+
 		int getNodeCount() const;
 		  // Gets the number of nodes in the tree.
 		  // Postcondition: Returns the number of nodes in the parse tree.
@@ -41,16 +39,25 @@ class ParseTree {
 		int dotNodeCount;  // Used to name the nodes in the dot file (each new node increments dotNodeCount).
 		vector<string> nodeDeclarations;
 		vector<string> edgeDeclarations;
+                void randomStupidFunction();
 		map<Node*, string> nodeNames;
 		map<string, string> varColors;
 		map<Node*, string> varIDs;
-		
-    private:
-		Node *root;   // Root node
-		void visitNode(Node* node);
-		void visitEdge(Node* parent, int childNumber);
-		void destroy(Node* &node); // Used by the destructor to free memory.
+                void visitNode(Node* node);
+                void visitEdge(Node* parent, int childNumber);
+                void destroy(Node* &node); // Used by the destructor to free memory.
+                Node* getRootNode();
+        private:
+                Node *root;   // Root node
+
 };
+
+//class Graph{
+//    public:
+//        static ParseTree* pTree;
+//
+//
+//};
 
 #endif
 
