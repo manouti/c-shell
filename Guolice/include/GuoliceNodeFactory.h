fe32fddb0fb6433f1b56cdde225e485f3d520171
lama.ghusn <lama.ghusn@d2ccdeb4-faeb-767e-6b92-2c8aa4952157>
2011-01-07 16:52:10 +0000

    translate

---
+++ Guolice/include/GuoliceNodeFactory.h
@@ -0,0 +1,24 @@
+#ifndef GUOLICENODEFACTORY_H
+#define GUOLICENODEFACTORY_H
+
+#include "AbstractNodeFactory.h"
+#include "ParseTree.h"
+
+class GuoliceNodeFactory : public AbstractNodeFactory
+{
+private:
+    map<progGraph::ProgNode*, Node*> progNode_Node_Map;
+
+public:
+    GuoliceNodeFactory();
+
+    void createNode(progGraph::ProgNode* node);
+
+    Node* getProgNodeMap(progGraph::ProgNode* progNode);
+
+
+
+
+
+};
+#endif // GuoliceNodeFactory_H
