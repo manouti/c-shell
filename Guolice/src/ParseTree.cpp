53f6bc93d79b2aa0c6faec2eb4349520c942a369
lama.ghusn <lama.ghusn@d2ccdeb4-faeb-767e-6b92-2c8aa4952157>
2011-01-09 12:49:25 +0000

    update for translate

--- Guolice/src/ParseTree.cpp
+++ Guolice/src/ParseTree.cpp
@@ -5,6 +5,12 @@ ParseTree::ParseTree(Node *root)
 	this->root = root;
 	dotNodeCount = 0;
 }
+
+void ParseTree::setRootNode(Node* root){
+        this->root = root;
+        dotNodeCount = 0;
+
+}
 		  
 ParseTree::~ParseTree()
 {
@@ -51,68 +57,12 @@ int ParseTree::getNodeCount() const
 
 void ParseTree::visitNode(Node* node)
 {
-//	string nodeDot ("node");
-//
-//	ostringstream oss;
-//	oss << dotNodeCount++;
-//	nodeDot += oss.str();
-//
-//	nodeNames[node] = nodeDot; // Store the label of the node in the dot file, e.g. node13 in the map "nodeNames".
-//
-//	nodeDot += "[label=\"";
-//
-//	string nodeText(node->getValue());
-//    replace(nodeText.begin(), nodeText.end(), '\"', '\'' );   // Replace the double-quote by a single-quote in case of a string for it to
-//															  // to be added correctly in the dot layout text.
-//	nodeDot += nodeText + "\",";
-//
-//	switch(node->getType()) {
-//		case Node::VAR:
-//			{
-//			string variableColor;
-//
-//			string varID = varIDs[node];
-//
-//			if (varColors.find(varID) != varColors.end()) {
-//				variableColor = varColors[node->getValue()];
-//			}
-//			else {
-//				double red = (static_cast<double> (rand()) / static_cast<double> (RAND_MAX) + 1.0) / 2.0;
-//				double green = (static_cast<double> (rand()) / static_cast<double> (RAND_MAX) + 1.0) / 2.0;
-//				double blue = (static_cast<double> (rand()) / static_cast<double> (RAND_MAX) + 1.0) / 2.0;
-//
-//				oss.flush();
-//				oss << red << " " << green << " " << blue;
-//				variableColor = oss.str();
-//
-//				varColors[varID] = variableColor;
-//			}
-//
-//			nodeDot += "rank=sink,style=filled,color=\"" + variableColor +  "\"];\n";
-//			}
-//			break;
-//		case Node::CONST:
-//			nodeDot += "shape=box];\n";
-//			break;
-//		case Node::OP:
-//			 nodeDot += "shape=diamond];\n";
-//	}
-//
-//	nodeDeclarations.push_back(nodeDot);
-//	//dotFile << nodeDot;
+
 }
 
 void ParseTree::visitEdge(Node* parent, int childNumber)
 {
-//	string edgeDot (nodeNames[parent]);
-//	edgeDot += " -> " + nodeNames[parent->getChildren().at(childNumber)] + "[label=";
-//
-//	ostringstream oss;
-//	oss << childNumber + 1;
-//	edgeDot += oss.str() + "];\n";
-//
-//	edgeDeclarations.push_back(edgeDot);
-//	//dotFile << edgeDot;
+
 }
 
 void ParseTree::destroy(Node* &node)
@@ -128,4 +78,18 @@ void ParseTree::destroy(Node* &node)
 Node* ParseTree::getRootNode(){
     return root;
 
+}
+
+void ParseTree::setFunctionList(vector<FunctionNode*> funcList){
+    functionList = funcList;
+}
+
+void ParseTree::printTree(Node * node){
+    node->setVisited();
+    cout <<node->toString() << endl;
+        for(int i = 0; i < node->getChildren().size(); i++)
+        {
+            if(!node->getChildren().at(i)->isVisited() )
+                printTree(node->getChildren().at(i));
+        }
 }
